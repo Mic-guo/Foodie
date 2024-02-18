6210136ddee47e54d0896fe829abe8087ea1c228
nandansrikrishna <nandans@umich.edu>
2024-02-17 20:27:43 -0800

    Syncing with updated camera snapshot

---
+++ Foodie/FriendsView.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsView.swift
+//  Foodie
+//
+//  Created by Nandan Srikrishna on 2/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct FriendsView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
